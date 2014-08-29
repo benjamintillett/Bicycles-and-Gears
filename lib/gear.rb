ea8b7029853fafa86c80e10617bd474c428d5dfc
benjamintillett <benjamin.tillett@gmail.com>
2014-08-29 09:44:35 +0100

    refactor Gear to use a wheel Struct

--- lib/gear.rb
+++ lib/gear.rb
@@ -1,11 +1,10 @@
 class Gear 
-	attr_reader :chainring, :cog, :rim, :tire
+	attr_reader :chainring, :cog, :wheel
 	def initialize(chainring, cog, rim, tire)
 		@chainring 	= chainring
 		@cog 		= cog
-		@rim		= rim 
-		@tire		= tire
-	end
+		@wheel		= Wheel.new(rim,tire)
+	end	
 
 	def ratio 
 		chainring/ cog.to_f
@@ -13,11 +12,14 @@ class Gear
 
 	def gear_inches
 		#tire goes around rim twice for diameter
-		ratio * diameter
+		ratio * wheel.diameter
 	end
 
-	def diameter
-		rim + (tire * 2 )
+
+	Wheel = Struct.new(:rim, :tire) do 
+		def diameter
+			rim + (tire * 2 )
+		end
 	end
 
 end
